72d63d30fb42a0ab355e2f1dc6919c3e5a0f71d0
duffyjp <duffy.jp@gmail.com>
2014-12-16 15:59:36 -0600

    Moved git related functions into it's own file.  Changed how the base duffy.rb file requires stuff.

--- lib/duffy.rb
+++ lib/duffy.rb
@@ -1,29 +1,3 @@
-require "duffy/version"
-
-module Duffy
-
-
-  # I like to have a git log in the admin section of my websites.  I use these in my capistrano tasks to
-  # generate what I need and upload along with the deployment.
-
-  class Git
-    class << self
-
-      # Produce tab separated listing of current git log.
-      # Useful for displaying a development history page.
-      def log
-        `git log --pretty=format:"%ad%x09%an%x09%s" --date=short`
-      end
-
-      # I tend use the commit count / 1000.0 as a version for my applications.
-      # You wouldn't want to do that if you're building a gem used by others.
-      def count
-        `git rev-list HEAD --count`
-      end
-
-    end
-  end
-
-
-
-end
+require File.dirname(__FILE__) + "/duffy/version"
+require File.dirname(__FILE__) + "/duffy/git"
+require File.dirname(__FILE__) + "/duffy/string"
