7c925d917d084580c7498ec0e3a45af1242a4c8d
jpd800 <duffy.jp@gmail.com>
2015-04-01 11:31:43 -0500

    Added MacOS support to CPU/core/thread detection.

--- lib/duffy/system.rb
+++ lib/duffy/system.rb
@@ -11,18 +11,30 @@ module Duffy
       # How many Physical CPUs do you have.
       # Detected by counting unique physical IDs
       def cpus
-        File.read('/proc/cpuinfo').scan(/^physical id.*/).uniq.count rescue 1
+        case RUBY_PLATFORM
+          when /linux/  then File.read('/proc/cpuinfo').scan(/^physical id.*/).uniq.count rescue 1
+          when /darwin/ then `system -n hw.packages`.to_i rescue 1
+          else 1
+        end
       end
 
       # How many actual CPU cores do we have not including Hyperthreading
       # "cpu cores" in cpuinfo is on a per physical processor basis, so we multiply by the number of CPUs
       def cores
-        (File.read('/proc/cpuinfo').scan(/^cpu cores.*/).first.scan(/\d+$/).first.to_i rescue 1) * cpus
+        case RUBY_PLATFORM
+          when /linux/  then (File.read('/proc/cpuinfo').scan(/^cpu cores.*/).first.scan(/\d+$/).first.to_i rescue 1) * cpus
+          when /darwin/ then `sysctl -n hw.physicalcpu`.to_i rescue 1
+          else 1
+        end
       end
 
       # How many threads does the system have.
       def threads
-        File.read('/proc/cpuinfo').scan(/^processor\s*:/).size rescue 1
+        case RUBY_PLATFORM
+          when /linux/  then File.read('/proc/cpuinfo').scan(/^processor\s*:/).size rescue 1
+          when /darwin/ then `sysctl -n hw.ncpu`.to_i rescue 1
+          else 1
+        end
       end
 
     end
