0bfc21cae370839b13bb0d1e3bbde9a022c0b60c
jpd800 <duffy.jp@gmail.com>
2017-05-11 12:22:44 -0500

    Bumped version & deprecated the last DuffyHelper

--- lib/duffy/duffy_helper.rb
+++ lib/duffy/duffy_helper.rb
@@ -4,6 +4,7 @@ module DuffyHelper
   # It's just an <li> with a link inside it.
   # menu_tag('Help', help_path)   =>  <li><a href="/help">Help</a></li>
   def menu_tag(title, link, options = {})
+    ActiveSupport::Deprecation.warn('This method will be removed without replacement in future versions')
     content_tag(:li, link_to(title, link), options)
   end
 
